2379e59b2bbfb1b799bd067b86f0924dbe1f6585
FreyMax <ismailovads@gmail.com>
2016-08-21 17:18:01 -0700

    added plots for visualizing

--- vis.py
+++ vis.py
@@ -1,4 +1,5 @@
-import matplotlib as mpl
+#import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 #import retreive_data.py
@@ -19,3 +20,23 @@ votes = [];
 #     votes.append(row[2])
 
 obj = [(names.append(row[0]), population.append(row[1]),votes.append(row[2]) ) for row in data]
+
+ratio = []
+for ppl, vote in zip(population, votes):
+    ratio.append(ppl/vote)
+
+#plt.subplot(221)
+plt.plot(votes,population, 'ko')
+plt.ylabel('State population')
+plt.xlabel('State Electoral Votes')
+#plt.axis([0,20, 0, 1.5e7])
+plt.grid(True)
+plt.show()
+
+#plt.subplot(222)
+plt.plot(ratio)
+plt.ylabel('State population per vote')
+plt.yscale('linear')
+plt.axis([0,50, 0 ,max(ratio)+1e5])
+plt.grid(True)
+plt.show()
