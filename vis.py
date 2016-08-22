15ab3a4a76aa06f301fcd161421f1b28bdd2855b
FreyMax <ismailovads@gmail.com>
2016-08-21 19:03:38 -0700

    clean up

--- vis.py
+++ vis.py
@@ -1,39 +1,21 @@
-#import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-#import retreive_data.py
 
-##names, population, votes = np.loadtxt('state_data.csv', dtype= ['str','int','int'],delimiter=',', usecols = (0,1,2), unpack = True)
-#population, votes = np.loadtxt('state_data.csv', dtype= 'int',delimiter=',', usecols = (1,2), unpack = True)
-##names = np.loadtxt('state_data.csv', dtype= 'str',delimiter=',', usecols = (0), unpack = True)
+data = pd.read_csv('state_data.csv')
 
-data = pd.read_csv('state_data.csv').as_matrix()
+names = data.ix[:, 0]
+population = data.ix[:, 1]
+votes = data.ix[:, 2]
 
-names = [];
-population = [];
-votes = [];
+ratio = [ (ppl/vote) for ppl, vote in zip(population, votes) ]
 
-# for row in data:
-#     names.append(row[0])
-#     population.append(row[1])
-#     votes.append(row[2])
-
-obj = [(names.append(row[0]), population.append(row[1]),votes.append(row[2]) ) for row in data]
-
-ratio = []
-for ppl, vote in zip(population, votes):
-    ratio.append(ppl/vote)
-
-#plt.subplot(221)
 plt.plot(votes,population, 'ko')
 plt.ylabel('State population')
 plt.xlabel('State Electoral Votes')
-#plt.axis([0,20, 0, 1.5e7])
 plt.grid(True)
 plt.show()
 
-#plt.subplot(222)
 plt.plot(ratio)
 plt.ylabel('State population per vote')
 plt.yscale('linear')
